e4d6eaa0fb21f1ec44b83fc44c8897826fa84d95
tushil@moon <tushil@moontechnolabs.com>
2025-03-07 23:11:12 +0530

    src changes

--- src/index.js
+++ src/index.js
@@ -28,6 +28,10 @@ io.on("connection", (socket) => {
   });
 });
 
+app.get("/",(req,res)=>{
+  res.send("hello")
+})
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
